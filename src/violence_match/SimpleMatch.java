679a372b736e827d8868acb7d2337df296c44c89
tom_glb <gy_glb@npu.com>
2019-12-15 10:17:06 +0800

    暴力匹配字符串

---
+++ src/violence_match/SimpleMatch.java
@@ -0,0 +1,14 @@
+package violence_match;
+
+public class SimpleMatch {
+    public static int method(String s1,String s2){
+        int l1=s1.length();
+        int l2=s2.length();
+        for(int i=0;i<=l1-l2;i++){
+            int j;
+            for(j=0;j<l2;j++) if(s1.charAt(i+j)!=s2.charAt(j)) break;
+            if(j==l2) return i;
+        }
+        return -1;
+    }
+}
